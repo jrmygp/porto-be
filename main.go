a0835be40bf809302c3e8b1cb23b3c3342431832
jrmygp <fat.snorlax17@gmail.com>
2024-01-25 14:27:56 +0700

    Inital commit & mysql connection

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:password@tcp(127.0.0.1:3306)/porto_be?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("DB Connection failed!")
+	}
+
+	fmt.Println("Database connection successful!")
+}
